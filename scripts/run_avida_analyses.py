ffe0a577d1a22d80616ce1e63737b2dcef473eff
amlalejini <amlalejini@gmail.com>
2016-05-03 22:26:33 -0400

    Added other treatment configs to project. Starting automated avida analyze mode script.

---
+++ scripts/run_avida_analyses.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+"""
+This script will run avida in analysis mode for each treatment and each avida analysis script specified in the
+ param/settings.json file.
+"""
+
+def main():
+    """
+    Main script
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
