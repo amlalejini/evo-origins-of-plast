da22072f54df664c7972a8973219fc9b97e4a88f
amlalejini <amlalejini@gmail.com>
2016-05-03 23:50:01 -0400

    Working on automating avida analysis runs

--- scripts/run_avida_analyses.py
+++ scripts/run_avida_analyses.py
@@ -2,13 +2,39 @@
 """
 This script will run avida in analysis mode for each treatment and each avida analysis script specified in the
  param/settings.json file.
+
+Once running avida analyze mode for treatment, this script will move the data dumped (dumped in the config location)
+ to the specified analysis dump location.
 """
+import json, os, subprocess
 
 def main():
     """
     Main script
     """
-    pass
+    settings_fn = "param/settings.json"
+    settings = None
+    # Load settings from settings file
+    with open(settings_fn) as fp:
+        settings = json.load(fp)
+    # Pull out general locations of interest
+    experiment_loc = settings["experiment_data"]["base_location"]
+    treats_configs_loc = settings["avida_analysis_config"]["configs_loc"]
+    # Pull out some other settings
+    treats_to_analyze = settings["avida_analysis_config"]["treatments_to_analyze"]
+    analyses = settings["avida_analysis_config"]["analysis_scripts_to_run"]
+
+    for treatment in treats_to_analyze:
+        print "Analyzing %s" % treatment
+        treat_config = os.path.join(treats_configs_loc, treatment)
+        for ascript in analyses:
+            print "  Running %s" % ascript
+            return_code = subprocess.call("pwd", shell = True, cwd = treat_config)
+            # Build avida analyze command and run it
+            cmd = "./avida -set ANALYZE_FILE %s -a" % ascript
+            return_code = subprocess.call(cmd, shell = True, cwd = treat_config)
+            # Move analyze files to destination
+            
 
 if __name__ == "__main__":
     main()
