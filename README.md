8d6a03318388fc617734e83971b8967d5551cbe8
amlalejini <amlalejini@gmail.com>
2016-05-26 13:44:17 -0400

    Refactoring to take into account new way of submitting scripts. Updated run_avida_analyses to handle all treatments in flattened experiment directory. Updated ss_analysis_settings. Next task: update process_final_dominants.py

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # evo-origins-of-plast
 Repository for Evolutionary Origins of Phenotypic Plasticity Project (as of 05/03/2016)
+
+## Scripts
+ * mk_runlist.py -- Uses experiment specifications defined in param/ss_exp_specs.json to generate a run_list file to run the experiment. (Entire experiment ran from one run_list file)
+ * run_avida_analyses.py -- Uses ss_analysis_settings for input parameters. Given details about file locations, etc, this script will run avida in analyze mode using all specified analysis scripts on all specified treatments. Original submission arguments for each treatment are used when running avida in analyze mode for that treatment.  
+  * process_final_dominants.py -- This script processes analysis data generated from the 'final_dom_analyze.cfg' avida analyze script.
+    * Expected output:
+      * final_dominant_details.csv:
+        * For each final dominant genotype: is_plastic, is_optimal, trait values, abstracted phenotype code, lineage code sequence, lineage full phenotype sequence
+      * final_dominant_overview.csv:
+        * treatment, # reps plastic, # reps optimal
--- scripts/run_avida_analyses.py
+++ scripts/run_avida_analyses.py
@@ -12,51 +12,51 @@ def main():
     """
     Main script
     """
-    settings_fn = "param/stepping_stones_analysis_settings.json"
+    settings_fn = "param/ss_analysis_settings.json"
     settings = None
     # Load settings from settings file
     with open(settings_fn) as fp:
         settings = json.load(fp)
     # Pull out general locations of interest
     experiment_loc = settings["experiment_data"]["base_location"]
-    treats_configs_loc = settings["avida_analysis_config"]["configs_loc"]
+    configs_loc = settings["avida_analysis_config"]["avida_configs_loc"]
+    analysis_scripts_src = settings["avida_analysis_config"]["analysis_scripts_source"]
     # Pull out some other settings
-    treats_to_analyze = settings["avida_analysis_config"]["treatments_to_analyze"]
+    treats_to_analyze = settings["avida_analysis_config"]["treatments_to_test"]
     analyses = settings["avida_analysis_config"]["analysis_scripts_to_run"]
+
+    # Read run_list file for run settings for args by treatment
+    avida_cmd_args = None
+    with open(os.path.join(configs_loc, "run_list"), "r") as fp:
+        avida_cmd_args = {line.split(" ")[1].split("__")[0]:line.split("./avida")[-1].replace("-s $seed ", "").strip() for line in fp if "./avida" in line}
 
     for treatment in treats_to_analyze:
         print "Analyzing %s" % treatment
-        treat_config = os.path.join(treats_configs_loc, treatment, "configs")
         for ascript in analyses:
             print "  Running %s" % ascript
-            return_code = subprocess.call("pwd", shell = True, cwd = treat_config)
-
-            fp = None
-            args = ""
-            try:
-                fp = open(os.path.join(treat_config, "avida_args.info"), "r")
-            except:
-                print("Couldn't find arg specification. Will just assume defaults.")
-            else:
+            ascript_fpath = os.path.join(analysis_scripts_src, ascript)
+            ## BUILD ANALYSIS FILE ##
+            modified_ascript = ""
+            ascript_content = ""
+            with open(ascript_fpath, "r") as fp:
                 for line in fp:
-                    args = line
-                    break
-                fp.close()
-
-            # Build avida analyze command and run it
-            if args != "":
-                cmd = "./avida %s -set ANALYZE_FILE %s -a" % (args, ascript)
-            else:
-                cmd = "./avida -set ANALYZE_FILE %s -a" % ascript
-            return_code = subprocess.call(cmd, shell = True, cwd = treat_config)
-            # Move analyze files to destination
-            # mv treatment_config/data/analysis base_location/treatment
-            src = os.path.join(treat_config, "data", "analysis")
-            dest = os.path.join(experiment_loc, treatment)
-            #cmd = "rsync -r %s %s" % (src, dest)
-            #return_code = subprocess.call(cmd, shell = True, cwd = treat_config)
-            # remove crusty data
-            #return_code = subprocess.call("rm -rf %s" % src, shell = True, cwd = treat_config)
+                    if line.strip() == "SET d":
+                        modified_ascript += "SET d %s\n" % experiment_loc
+                    elif line.strip() == "FOREACH t":
+                        modified_ascript += "FOREACH t %s\n" % treatment
+                    else:
+                        modified_ascript += line
+            ## WRITE ANALYSIS FILE TO RUN LOCATION ##
+            with open(os.path.join(configs_loc, ascript), "w") as fp:
+                fp.write(modified_ascript)
+            ## BUILD ANALYSIS COMMAND ##
+            cmd = "./avida %s -a -set ANALYZE_FILE %s" % (avida_cmd_args[treatment], ascript)
+            ## RUN AVIDA ANALYSIS ##
+            #print cmd
+            #subprocess.call("pwd", shell = True, cwd = configs_loc)
+            return_code = subprocess.call(cmd, shell = True, cwd = configs_loc)
+            ## CLEAN UP ANALYSIS SCRIPT ##
+            return_code = subprocess.call("rm %s" % ascript, shell = True, cwd = configs_loc)
 
 if __name__ == "__main__":
     main()
